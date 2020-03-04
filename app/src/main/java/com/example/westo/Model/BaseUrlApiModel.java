88191cfd5b57879befb9f33bf8e466a627ddfdf3
Biem <Abiemassyikin@gmail.com>
2020-03-04 22:48:52 +0700

    gg

--- app/src/main/java/com/example/westo/Model/BaseUrlApiModel.java
+++ app/src/main/java/com/example/westo/Model/BaseUrlApiModel.java
@@ -1,8 +1,8 @@
 package com.example.westo.Model;
 
 public class BaseUrlApiModel {
-//    private String baseURL="http://64310715e142.sn.mynetname.net/api_siputu/";
-    private String baseURL="http://siputu.000webhostapp.com/";
+    private String baseURL="http://64310715e142.sn.mynetname.net/api_siputu/";
+//    private String baseURL="http://siputu.000webhostapp.com/";
 
     public String getBaseURL() {
         return baseURL;
