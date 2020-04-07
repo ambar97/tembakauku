324f42a246d9f0bfed18fb34400c98f1e842a28c
ambar97 <abiemassyikin@gmail.com>
2020-04-07 12:55:45 +0700

    minus burley

--- app/src/main/java/com/example/westo/Data_Mutu.java
+++ app/src/main/java/com/example/westo/Data_Mutu.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Data_Mutu extends AppCompatActivity {
+public class xData_Mutu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
